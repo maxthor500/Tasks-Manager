da6b6b0438b7d47745f71db914ddabe76a28b83d
maxthor500 <iraubuntu@gmail.com>
2021-03-30 21:18:46 +0000

    Add categories dropdown

--- app.py
+++ app.py
@@ -101,10 +101,12 @@ def logout():
 
 @app.route("/add_task")
 def add_task():
-    return render_template("add_task.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_task.html", categories=categories)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+

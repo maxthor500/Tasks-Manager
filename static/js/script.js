18e4b74b27e7454608bdb49fe7a1e3ba1d5883eb
maxthor500 <iraubuntu@gmail.com>
2021-03-30 19:15:19 +0000

    Add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
+/*
+    jQuery for MaterializeCSS initialization
+*/
+
+$(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-  });
+    $(".collapsible").collapsible();
+});
